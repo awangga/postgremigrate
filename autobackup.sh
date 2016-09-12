9f2d3524a68aade37447920401e2826c27abd38b
Rolly Maulana Awangga <rolly@awang.ga>
2016-09-12 17:08:52 +0700

    add to crontab

--- autobackup.sh
+++ autobackup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 ##create .pgpass file in home dir with 600 permission and field with hostname:port:database:username:password
+##create crontab 0 5 * * 1 ~/pg_dump.sh
+
 a=`date +"%y%m%d%H%M%S"`"db_non_pertandingan.sql.gz"
 pg_dump -h 10.16.9.86 -U postgres db_non_pertandingan | gzip > ~/$a
 
